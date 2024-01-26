089d4d8066487a03541b3625503b74dd9a578e7e
Mafe-Garcia <mariaf.garcia@upb.edu.co>
2024-01-25 20:48:36 -0500

    muchos textos

---
+++ otrotexto.c
@@ -0,0 +1,6 @@
+#include <stdio.h>#include <stdlib.h>int main()
+
+{
+    printf("no c cuantos hay que hacer\n");
+    return(EXIT_SUCCESS);
+}
---
+++ ultimotexto.c
@@ -0,0 +1,6 @@
+#include <stdio.h>#include <stdlib.h>int main()
+
+{
+    printf("4 es de mala suerte\n");
+    return(EXIT_SUCCESS);
+}
